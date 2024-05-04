cbb66ffc13586a6345c3415e33415da801a0a892
Julien Angelo Yoganathan <julienangeloyoganathan@gmail.com>
2024-05-05 01:00:18 +0530

    commiting changes upto data access layer setup

--- demo/demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,11 +11,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentrepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentrepository) {
+        this.studentrepository = studentrepository;
+    }
+
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(1L,"Jude Mary","judemary@gmail.com",LocalDate.of(2000, Month.JULY,9),23),
-                new Student(2L,"Mary Anne","maryanne@gmail.com",LocalDate.of(1998,Month.SEPTEMBER,4),25),
-                new Student(3L,"Sam Curran","samcurran@gmail.com",LocalDate.of(2001,Month.MARCH,9),23)
-        );
+        return studentrepository.findAll();
     }
 }
